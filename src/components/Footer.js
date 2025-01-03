7e53fe14b965037ede1bf510f5e82cf049a7b53d
OniiCode <oniicoder@gmail.com>
2025-01-03 16:15:20 +0000

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import Socials from "./Socials"
 export default function Footer() {
     return (
         <div className="px-5 flex flex-col lg:flex-row flex-col-reverse justify-between gap-6 items-center py-6 text-sm lg:text-base text-dull-gray text-center">
-            <div>&copy; Copyright Oniicode, {new Date().getFullYear()}. All rights reserved. </div>
+            <div>&copy; Copyright Oniicode, {new Date().getFullYear()}. All rights reserved.</div>
             <Socials />
         </div>
     )
